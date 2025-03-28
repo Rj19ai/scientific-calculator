c1c163d4b38a01dffede8a017771d13a46cd7b96
rj19ai <rahul.raj@iiitb.ac.in>
2025-03-28 20:17:29 +0530

    Updated Python file

--- sc_calculator.py
+++ sc_calculator.py
@@ -40,4 +40,4 @@ if __name__ == "__main__":
         print(f"Power: {power(base, exponent)}")
 
     else:
-        print("Exiting program. Thanks!")
+        print("Exiting program. Thanks again!!")
